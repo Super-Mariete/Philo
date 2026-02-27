7256d010d58ab3ce37e347129865b2fa3be69162
Mario <mario@MacBook-Pro-de-Maeva.local>
2026-02-27 19:51:28 +0100

    dividida monitor

--- SRC/monitor.c
+++ SRC/monitor.c
@@ -6,12 +6,12 @@
 /*   By: made-ped <made-ped@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 20:31:43 by made-ped          #+#    #+#             */
-/*   Updated: 2026/02/25 21:36:39 by made-ped         ###   ########.fr       */
+/*   Updated: 2026/02/27 19:49:35 by made-ped         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INC/philosopher.h"
-
+/*
 void	*monitor_routine(void *arg)
 {
 	t_data	*data;
@@ -53,6 +53,49 @@ void	*monitor_routine(void *arg)
 			return (NULL);
 		}
 		usleep (1000);
+	}
+	return (NULL);
+}*/
+
+static int	check_philo(t_data *data, int i, int *full)
+{
+	long	last_meal;
+
+	pthread_mutex_lock(&data->philos[i].meal_mutex);
+	last_meal = data->philos[i].last_meal;
+	if (data->must_eat != -1 && data->philos[i].meals_eaten < data->must_eat)
+		*full = 0;
+	pthread_mutex_unlock(&data->philos[i].meal_mutex);
+	if (get_time() - last_meal > data->time_die)
+	{
+		print_status(&data->philos[i], "died");
+		stop_simulation(data);
+		return (0);
+	}
+	return (1);
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_data	*data;
+	int		i;
+	int		full;
+
+	data = (t_data *)arg;
+	usleep(1000);
+	while (get_simulation_state(data))
+	{
+		i = -1;
+		full = 1;
+		while (++i < data->nb_philo)
+			if (!check_philo(data, i, &full))
+				return (NULL);
+		if (data->must_eat != -1 && full)
+		{
+			stop_simulation(data);
+			return (NULL);
+		}
+		usleep(1000);
 	}
 	return (NULL);
 }
